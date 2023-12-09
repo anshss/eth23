8453c69da574509d75586630c901637ec24eba83
bruh <nkhan054002@gmail.com>
2023-12-09 13:36:25 +0530

    bwn frontend

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -1,19 +1,21 @@
 import React from "react";
 import styled from "styled-components";
-
-const Navbar = styled.div`
-  background-color: #311432; /* Your desired purple color */
-  padding: 1em;
-  color: white;
-  display: flex;
-  justify-content: center;
-`;
+import Link from "next/link";
 
 export default function NavBar() {
   return (
-    <Navbar className="navbar">
-      {/* Wrap your content in another element with the desired style */}
-      <div>DeSo by TBAI </div>
-    </Navbar>
+<nav className="fixed h-[75px] top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+<div className="px-3 py-3 lg:px-5 lg:pl-3">
+    <div className="flex items-center justify-between">
+        <div className="flex items-center justify-start">
+          <h2>TBAI</h2>
+        </div>
+        <div className="flex items-center">
+            <div className="flex items-center ml-3">
+            </div>
+        </div>
+    </div>
+</div>
+</nav>
   );
 }
--- app/components/SideBar.js
+++ app/components/SideBar.js
@@ -1,31 +1,64 @@
-import React from "react";
-import styled from "styled-components"
-
-const StyledLink = styled.a`
-  color: violet
-  text-decoration: none;
-  cursor: pointer;
-  height:15px;
-  background-color: #311432;
-  padding: 1.09cm
-`;
-
-const Sidebar = styled.div`
-    display: flex;
-    flex-direction: column;
-    width: 200px;
-    background-color: #311432;
-    height: 100vh;
-`
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { usePathname } from "next/navigation";
 
 export default function SideBar () {
+    const pathname = usePathname();
+    const id = pathname.split("/")[2];
 
-    return (<Sidebar className="sidebar">
-        <StyledLink className="asd" href="/dashboard/createmodel">Become a Desoite</StyledLink>
-        <StyledLink href="/dashboard/analytics">Desoite Details</StyledLink>
-        <StyledLink href="/dashboard/fetchmodels">Desoites</StyledLink>
-        <StyledLink href="/dashboard/createcontent">Desopost</StyledLink>
-        <StyledLink href="/notification">push testing</StyledLink>
-        <StyledLink href="/dashboard/logout">Become boring</StyledLink>
-    </Sidebar>)
+    const isActive = (_pathname) => {
+      return id === _pathname;
+    };
+
+    return (
+        <aside
+        id="logo-sidebar"
+        className="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
+        aria-label="Sidebar"
+      >
+        <div className="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
+          <ul className="space-y-2 font-medium">
+            <li>
+              <Link
+                href="/dashboard/modelgen"
+                className={"flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 " + (isActive("/dashboard/create") ?"bg-gray-700" : "")}
+              >
+                  <path d="M453 776h60V610h167v-60H513V376h-60v174H280v60h173v166Zm27.266 200q-82.734 0-155.5-31.5t-127.266-86q-54.5-54.5-86-127.341Q80 658.319 80 575.5q0-82.819 31.5-155.659Q143 347 197.5 293t127.341-85.5Q397.681 176 480.5 176q82.819 0 155.659 31.5Q709 239 763 293t85.5 127Q880 493 880 575.734q0 82.734-31.5 155.5T763 858.316q-54 54.316-127 86Q563 976 480.266 976Zm.234-60Q622 916 721 816.5t99-241Q820 434 721.188 335 622.375 236 480 236q-141 0-240.5 98.812Q140 433.625 140 576q0 141 99.5 240.5t241 99.5Zm-.5-340Z" />
+                <span className="flex-1 ml-3 whitespace-nowrap">Become a Desoite</span>
+              </Link>
+            </li>
+  
+            <li>
+              <Link
+                href="/dashboard/adgen"
+                className={"flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 " + (isActive("/dashboard/create") ?"bg-gray-700" : "")}
+              >
+                  <path d="M453 776h60V610h167v-60H513V376h-60v174H280v60h173v166Zm27.266 200q-82.734 0-155.5-31.5t-127.266-86q-54.5-54.5-86-127.341Q80 658.319 80 575.5q0-82.819 31.5-155.659Q143 347 197.5 293t127.341-85.5Q397.681 176 480.5 176q82.819 0 155.659 31.5Q709 239 763 293t85.5 127Q880 493 880 575.734q0 82.734-31.5 155.5T763 858.316q-54 54.316-127 86Q563 976 480.266 976Zm.234-60Q622 916 721 816.5t99-241Q820 434 721.188 335 622.375 236 480 236q-141 0-240.5 98.812Q140 433.625 140 576q0 141 99.5 240.5t241 99.5Zm-.5-340Z" />
+  
+                <span className="flex-1 ml-3 whitespace-nowrap">Desopost</span>
+              </Link>
+            </li>
+            <li>
+            <Link
+                href="/dashboard/ads"
+                className={"flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 " + (isActive("/dashboard/nfts") ?"bg-gray-700" : "")}
+              >
+                  <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z" />
+                  <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z" />
+                <span className="flex-1 ml-3 whitespace-nowrap">Ads</span>
+              </Link>
+            </li>
+            <li>
+            <Link
+                href="/dashboard/marketplace"
+                className={"flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 " + (isActive("/dashboard/nfts") ?"bg-gray-700" : "")}
+              >
+                  <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z" />
+                  <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z" />
+                <span className="flex-1 ml-3 whitespace-nowrap">Become Boring</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </aside>)
 }
